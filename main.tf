8e00d18ca250cac1a5b5655051d9419e0de5f22f
zmoham1 <zmohamm1@umbc.edu>
2025-11-03 11:52:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-lab7a-zm"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock-zm"
-  }
-}
+
